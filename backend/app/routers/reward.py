c0549c55073efa51e2fcfe96666ec92ce10fd9ce
Krynovv <danilkrylol@gmail.com>
2026-07-06 15:09:04 +0300

    servers_v-0.0.6b

--- backend/app/routers/reward.py
+++ backend/app/routers/reward.py
@@ -12,29 +12,29 @@ from ..service.economy import purchase_reward
 router = APIRouter(prefix="/rewards", tags=["rewards"])
 
 @router.get("", response_model=list[RewardRead])
-asyncd def list_rewards (
+async def list_rewards (
     db: AsyncSession = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
     result = await db.execute(select(Reward).where(Reward.user_id == current_user.id))
     return result.scalars().all()
 
-@router.post("", response_model=RewardRead, status_code=statis.HTTP_201_CREATED)
+@router.post("", response_model=RewardRead, status_code=status.HTTP_201_CREATED)
 async def created_reward(
     data: RewardCreate,
     db: AsyncSession = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    reward = Reward(User_id=current_user.id, **data.model_dump())
+    reward = Reward(user_id=current_user.id, **data.model_dump())
     db.add(reward)
     await db.commit()
     await db.refresh(reward)
     return reward
 
-@router.post("/{reward.id}/purchase", response_model=RewardRead)
+@router.post("/{reward_id}/purchase", response_model=RewardRead)
 async def purchase_reward_endpoint(
     reward_id: int,
     db: AsyncSession = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    return await purchase_reward(db, current_user.id, reward.reward_id)
+    return await purchase_reward(db, current_user.id, reward_id)
